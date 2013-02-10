58c11132b045fa1ccc0859be052c62c96e1415f7
Michael Thorpe <mt2309@ic.ac.uk>
2013-02-10 17:02:48 +0000

    Rename output file and refactor main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -8,20 +8,33 @@ import Conversions._
  */
 object Main extends App {
 
-  val help = "Argument 1: directory containing .pfm files of the same resolution"
+  val help = "Argument 1: mode, Argument 1: directory containing .pfm files of the same resolution"
 
-  assert(args.length > 0,help)
+  assert(args.length > 1,help)
 
-  val images = getFiles(new File(args(0))) map(x => (new Reader(x)).image)
+  args(0) match {
+    case "assemble" | "HDR" | "hdr" | "1" | "one" => hdr
+    case "lighting" | "image-based-lighting" | "2" | "two" => lighting
+    case _ => {println(help); sys.exit(1)}
+  }
 
-  assert(images.length > 0, help)
+  private def hdr {
+    val images = getFiles(new File(args(1))) map (x => (new Reader(x)).image)
 
-  // check the files are the same resolution
-  images foreach(x => assert(x.length == images(0).length &&
-    (x(0).length == images(0)(0).length),"Image is not the correct resolution, " ++ help))
+    assert(images.length > 0, help)
+
+    // check the files are the same resolution
+    images foreach (x => assert(x.length == images(0).length &&
+      (x(0).length == images(0)(0).length), "Image is not the correct resolution, " ++ help))
 
 
-  val exposures = new Writer((new ExposureHandler(images)).reduced,"f.pfm")
+    new Writer((new ExposureHandler(images)).reduced, "f.pfm")
+  }
+
+  private def lighting {
+    val image = new Reader(new File(args(1)))
+  }
+
 
   def getFiles(directory:File):Array[File] = {
     directory listFiles(new FilenameFilter {
