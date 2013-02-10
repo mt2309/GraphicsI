b78d99868c1b66320f0c8c98ac1ea91560dbbaa1
Michael Thorpe <mt2309@ic.ac.uk>
2013-02-10 18:03:32 +0000

    Little bit more work on lighting

--- src/main/scala/Lighting.scala
+++ src/main/scala/Lighting.scala
@@ -8,7 +8,12 @@ final class Lighting(image:Image) {
   // Check we have a square image
   assert(image.xDim == image.yDim)
 
-  private val center = (image.xDim/2,image.yDim/2)
+  private val center = image.xDim/2
+  private val radius = image.xDim/2
+
+
+
+  private def inCircle(x:Int,y:Int):Boolean = Math.pow(x - center,2) + Math.pow(y - center,2) < Math.pow(radius,2)
 
 
 
