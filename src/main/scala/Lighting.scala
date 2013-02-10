df4288ce9c8d35db1078825b971c5496c1b3c0c1
Michael Thorpe <mt2309@ic.ac.uk>
2013-02-10 17:03:29 +0000

    Start to question 2

---
+++ src/main/scala/Lighting.scala
@@ -0,0 +1,15 @@
+/**
+ * User: mthorpe
+ * Date: 09/02/2013
+ * Time: 22:39
+ */
+final class Lighting(image:Image) {
+
+  // Check we have a square image
+  assert(image.xDim == image.yDim)
+
+  private val center = (image.xDim/2,image.yDim/2)
+
+
+
+}
